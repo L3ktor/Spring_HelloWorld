1c77721d316fbf618875c0d0d571f85f6624a8d3
L3ktor <goopapota@gmail.com>
2024-04-23 12:37:28 +0300

    Complete the quiz
    Signed-off-by: L3ktor <goopapota@gmail.com>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat bCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(bCat.getMessage());
+
+        Cat bCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bCat2.getMessage());
+
+        System.out.println(bean == bean2);
+        System.out.println(bCat == bCat2);
     }
 }
